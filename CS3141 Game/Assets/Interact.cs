f7142615ec75bc001a5af82e1ceabec1d00f4aef
bhawes24 <64669597+bhawes24@users.noreply.github.com>
2022-04-20 17:55:09 -0400

    GoOutsideMenu Part 3

--- CS3141 Game/Assets/Interact.cs
+++ CS3141 Game/Assets/Interact.cs
@@ -26,49 +26,35 @@ public class Interact : MonoBehaviour
     public int timeCostHours = 0;
     public int timeCostMinutes = 0;
     public int energyMod = 0; //used so you can sleep when energy == 0
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (inRange) 
+        if (inRange)
         {
-            if(Input.GetKeyDown(interactKey)) //true if interact key (space) is pressed down
+            if (Input.GetKeyDown(interactKey)) //true if interact key (space) is pressed down
             {
                 if (stats.energy.getStat() + energyMod == 0) { ShowMessage("You need to sleep"); }
 
                 else
                 {
-                    stats.energy.changeStat(energyInc);
-                    stats.intelligence.changeStat(intelligenceInc);
-                    stats.health.changeStat(healthInc);
-                    stats.social.changeStat(socialInc);
-                    stats.drunkenness.changeStat(drunkennessInc);
-                    stats.decisionMaking.changeStat(decisionMakingInc);
-                    stats.athleticness.changeStat(athleticnessInc);
-                    stats.coldness.changeStat(coldnessInc);
-                    stats.money.changeStat(moneyInc);
-
-                    timeClock.passTime(timeCostHours, timeCostMinutes);
-
-                    ShowMessage(resultMessage);
-
-                    interact.Invoke(); //invoke event
+                    doTheThing();
                 }
-                
+
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) 
+        if (collision.gameObject.CompareTag("Player"))
         {
             inRange = true;
             ShowMessage(interactMessage);
@@ -93,4 +79,22 @@ public class Interact : MonoBehaviour
             displayedText.GetComponent<TextMesh>().text = text;
         }
     }
+
+    public void doTheThing() {
+        stats.energy.changeStat(energyInc);
+        stats.intelligence.changeStat(intelligenceInc);
+        stats.health.changeStat(healthInc);
+        stats.social.changeStat(socialInc);
+        stats.drunkenness.changeStat(drunkennessInc);
+        stats.decisionMaking.changeStat(decisionMakingInc);
+        stats.athleticness.changeStat(athleticnessInc);
+        stats.coldness.changeStat(coldnessInc);
+        stats.money.changeStat(moneyInc);
+
+        timeClock.passTime(timeCostHours, timeCostMinutes);
+
+        ShowMessage(resultMessage);
+
+        interact.Invoke(); //invoke event
+    } 
 }
