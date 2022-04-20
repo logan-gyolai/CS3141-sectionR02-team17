512a9c39af522643808718a8b62eafbb1a5f097e
bhawes24 <64669597+bhawes24@users.noreply.github.com>
2022-04-20 17:23:59 -0400

    GoOutsideMenu Part 1

---
+++ CS3141 Game/Assets/GoOutsideMenu.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class GoOutsideMenu : MonoBehaviour
+{
+    public GameObject goOutsideMenu;
+    public bool isInMenu = false;
+    public KeyCode interactKey;
+    public bool inRange;
+    [SerializeField] string interactMessage;
+    [SerializeField] GameObject displayedText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goOutsideMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                if (isInMenu)
+                {
+                    ResumeGame();
+                }
+                else
+                {
+                    PauseGame();
+                }
+            }
+        }
+        
+    }
+
+    public void PauseGame()
+    {
+        goOutsideMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isInMenu = true;
+    }
+
+    public void ResumeGame()
+    {
+        goOutsideMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isInMenu = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            inRange = true;
+            ShowMessage(interactMessage);
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            inRange = false;
+            ShowMessage("");
+            Debug.Log("Player not in range");
+        }
+    }
+
+    public void ShowMessage(string text)
+    {
+        if (displayedText)
+        {
+            displayedText.GetComponent<TextMesh>().text = text;
+        }
+    }
+}
+
